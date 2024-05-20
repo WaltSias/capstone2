e378808503b75012531002dda98dc94d582e0346
WaltSias <walt.sias@gmail.com>
2024-05-20 14:35:42 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# capstone2
-project2
+# Capstone2
+
+This is a website built with Bootstrap/CSS/Html/JavaScript/API.
+
+## About
+
+It contains a page for Tennis stats:
+
+- Player info
+
+## Built With
+
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+
+## Roadmap
+
+- [x] Make a Branch
+- [x] Html
+- [x] CSS
+- [x] JS
+- [x] Bootstrap
+
+## Wireframe
+
+![wireframe]()
+
+Contact
+
+**Author: Walter Sias, walt.sias@gmail.com**
+
+**Acknowledgements**: Added html files, bootstrap CDN, also custom boostrap from w3 schools, and I used float and responsive utlities.
+
+- [Bootstrap](https://getbootstrap.com/)
+- [w3schoools CSS](https://www.w3schools.com/cssref)
+- [w3schools JS](https://www.w3schools.com/js/)
+- [rapidapi](https://rapidapi.com/sportcontentapi/api/tennis-live-data/)
---
+++ js/stats.js
@@ -0,0 +1,63 @@
+document
+  .getElementById('tennisForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const apiKey = 'a7a7a69c6fmshab1990686aa6873p1825c7jsnc11a01826fab';
+
+
+
+    const fetch = require('node-fetch');
+
+const url = 'https://tennis-live-data.p.rapidapi.com/rankings/ATP';
+const options = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': 'a7a7a69c6fmshab1990686aa6873p1825c7jsnc11a01826fab',
+    'x-rapidapi-host': 'tennis-live-data.p.rapidapi.com'
+  }
+};
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const player = data.data.find(
+          (player) =>
+            player.first_name === firstName && player.last_name === lastName
+        );
+        const playerInfoDiv = document.getElementById('playerInfo');
+
+        if (player) {
+          const playerHtml = `
+                  <h2>Player Information</h2>
+                  <p>country: ${player.country}</p>
+                  <p>Name: ${player.first_name} ${player.full_name}</p>
+                  <p>ID: ${player.id}</p>
+                  <p>Last Name: ${player.last_name}</p>
+                  
+              `;
+          playerInfoDiv.innerHTML = playerHtml;
+        } else {
+          playerInfoDiv.innerHTML = '<p>Player not found</p>';
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+function clearData() {
+  document.getElementById('playerInfo').innerHTML = '';
+  document.getElementById('firstName').value = '';
+  document.getElementById('lastName').value = '';
+}
+document
+  .getElementById('clearButton')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    clearData();
+  });
